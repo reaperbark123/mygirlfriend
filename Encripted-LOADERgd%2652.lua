62007563cfcfe2a45de6227cf9c99a79ea4994b1
reaperbark123 <88252453+reaperbark123@users.noreply.github.com>
2024-05-03 18:52:51 -0500

    Update and rename Encripted-LOADERgd%2652 to Encripted-LOADERgd%2652.lua

---
+++ Encripted-LOADERgd%2652.lua
@@ -0,0 +1,7 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/reaperbark123/mygirlfriend/masterrr/TEEHEE"))();
+local Id = game.GameId
+
+if Id == 1958807588 then
+  repeat task.wait() until getgenv().ImageLoaded == true
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/reaperbark123/mygirlfriend/masterrr/Encripted-MGKFKG42351%25%24525F"))();
+end;
